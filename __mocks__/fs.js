f5872b863b9eeaff7e4231161d136b1a3d6e92d9
朱骏杰 <ories.zhu@mochain.vip>
2020-06-15 23:06:01 +0800

    read测试完毕

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,7 +1,27 @@
 const fs = jest.genMockFromModule('fs'); // 第二步在这里写上genMockFromModule,这是个约定
-fs.x = ()=>{
-  console.log('hi')
-  return 'xxx'
+const _fs = jest.requireActual('fs'); // 以前实际的模块
+
+Object.assign(fs, _fs) // 把_fs key 值 复制 到左边
+
+const mocks = {}
+
+fs.setMock = (path,error,data)=>{
+  mocks[path] = [error, data] // 返回
 }
+
+fs.readFile= (path, options, callback)=>{ // 覆盖原来的
+
+  // fs.readFile('xxx',fn)
+  if(callback === undefined){callback = options}
+
+  if(path in mocks) {
+    // callback(mocks[path][0],mocks[path][1]) // 如果发现是被mock过的就不走真正的readfile
+    callback(...mocks[path])
+  }else {
+    _fs.readFile(path, options, callback)
+  }
+
+}
+
 
 module.exports = fs
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,12 +3,11 @@ const fs = require('fs') // 这个是真的fs
 jest.mock('fs') // 这里变成了假的fs,第一步在测试的地方mock
 
 describe('db',()=>{ // describe 描述一个东西
-it('can read', ()=>{ // it 它能做什么
+it('can read', async ()=>{ // it 它能做什么
     // expect(db.read instanceof Function).toBe(true) // expect 也要非常熟悉
-    // fs.x()
-    expect(fs.x()).toBe('xxx')
+  const data = [{title:'hi',done:true}]
+  fs.setMock('/xxx',null,JSON.stringify(data))
+  const list = await db.read('/xxx')
+  expect(list).toStrictEqual(data) // 对比两个对象toStrictEqual
   })
-  // it('can write', ()=>{
-  //   expect(db.read instanceof Function).toBe(true)
-  // })
 })
