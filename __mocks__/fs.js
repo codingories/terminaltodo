700f126687c2307d93fde33191d98ce593925518
朱骏杰 <ories.zhu@mochain.vip>
2020-06-16 16:27:47 +0800

    开始解决undefined的问题

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,26 +3,40 @@ const _fs = jest.requireActual('fs'); // 以前实际的模块
 
 Object.assign(fs, _fs) // 把_fs key 值 复制 到左边
 
-const mocks = {}
+const readMocks = {}
 
-fs.setMock = (path,error,data)=>{
-  mocks[path] = [error, data] // 返回
+fs.setReadFileMock = (path,error,data)=>{
+  readMocks[path] = [error, data] // 返回
   // mocks['/xxx'] = null, [{title:'hi',done:true}]
 }
 
-fs.readFile= (path, options, callback)=>{ // 覆盖原来的
-
+fs.readFile = (path, options, callback)=>{ // 覆盖原来的
   // fs.readFile('xxx',fn)
   if(callback === undefined){callback = options}
-
-  if(path in mocks) {
+  if(path in readMocks) {
     // callback(mocks[path][0],mocks[path][1]) // 如果发现是被mock过的就不走真正的readfile
-    callback(...mocks[path])
+    callback(...readMocks[path])
   }else {
     _fs.readFile(path, options, callback)
   }
-
 }
 
+const writeMocks = {}
+
+fs.setWriteFileMock = (path, fn)=>{ // fn 表示该函数应该做什么
+  console.log('setWriteFileMock',path,fn)
+  writeMocks[path] = fn
+  console.log(',shit',writeMocks)
+}
+
+
+fs.writeFile = (path, data, options, callback)=>{
+  if(callback === undefined){callback = options}
+  if(path in writeMocks){
+    writeMocks[path](path, data, options, callback)
+  } else {
+    _fs.writeFile(path, data, options, callback) // 其实就是调用真正的readfile还是mock
+  }
+}
 
 module.exports = fs
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -6,8 +6,22 @@ describe('db',()=>{ // describe 描述一个东西
 it('can read', async ()=>{ // it 它能做什么
     // expect(db.read instanceof Function).toBe(true) // expect 也要非常熟悉
   const data = [{title:'hi',done:true}]
-  fs.setMock('/xxx',null,JSON.stringify(data))
+  fs.setReadFileMock('/xxx',null,JSON.stringify(data))
   const list = await db.read('/xxx')
   expect(list).toStrictEqual(data) // 对比两个对象toStrictEqual
+  })
+  it('can write', async () => {
+    let fakeFile
+    fs.setWriteFileMock('/yyy', (path, data, callback)=>{
+      fakeFile = data // 往yyy写东西，不要写到yyy, 直接写到fake file
+      callback(null)
+    })
+    console.log('fuck fakeFile1',fakeFile)
+    const list = [{title:'t',done:true},{title:'e',done: true}]
+    console.log('JSON.stringify(list)',JSON.stringify(list))
+    await db.write(list, '/yyy')
+    console.log('fuck fakeFile2',fakeFile)
+
+    expect("sdf").toBe((JSON.stringify(list)))
   })
 })
